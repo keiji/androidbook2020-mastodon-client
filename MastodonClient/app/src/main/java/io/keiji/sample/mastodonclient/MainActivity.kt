3e5b1fedd4f844eb32aa9f8efdfb9569093fc48f
Keiji ARIYAMA <keiji_ariyama@c-lis.co.jp>
2020-03-07 11:07:18 +0900

    Step3:表示内容を変更する

--- MastodonClient/app/src/main/java/io/keiji/sample/mastodonclient/MainActivity.kt
+++ MastodonClient/app/src/main/java/io/keiji/sample/mastodonclient/MainActivity.kt
@@ -2,11 +2,15 @@ package io.keiji.sample.mastodonclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textview)
+        textView.text = "Hello XML Layout!"
     }
 }
