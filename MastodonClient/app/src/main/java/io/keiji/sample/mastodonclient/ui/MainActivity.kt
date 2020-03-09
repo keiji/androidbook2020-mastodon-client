c3d0033793eb0899071e1220a6441e868481e318
Keiji ARIYAMA <keiji_ariyama@c-lis.co.jp>
2020-03-09 16:45:25 +0900

    Step30:下メニュー(BottomNavigation)を追加する

--- MastodonClient/app/src/main/java/io/keiji/sample/mastodonclient/ui/MainActivity.kt
+++ MastodonClient/app/src/main/java/io/keiji/sample/mastodonclient/ui/MainActivity.kt
@@ -2,14 +2,36 @@ package io.keiji.sample.mastodonclient.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import io.keiji.sample.mastodonclient.R
+import io.keiji.sample.mastodonclient.databinding.ActivityMainBinding
 import io.keiji.sample.mastodonclient.ui.toot_list.TootListFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            val fragment = when (it.itemId) {
+                R.id.menu_home -> TootListFragment()
+                R.id.menu_public -> TootListFragment()
+                else -> null
+            }
+            fragment ?: return@setOnNavigationItemSelectedListener false
+
+            supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.fragment_container,
+                    fragment,
+                    TootListFragment.TAG
+                )
+                .commit()
+
+            return@setOnNavigationItemSelectedListener true
+        }
 
         if (savedInstanceState == null) {
             val fragment = TootListFragment()
